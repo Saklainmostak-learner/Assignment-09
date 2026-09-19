0b5b4c01f2c1c1fde0cb750788d0a145080c4fad
Saklainmostak-learner <saqlainmustaksaqi1@gmail.com>
2026-09-19 23:28:30 +0600

    clean and aplly css on failities page

--- src/pages/Facilities.jsx
+++ src/pages/Facilities.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-
 const Facilities = () => {
-    return (
-        <main>
-           <span className="webbrow">DISCOVER</span> 
-           <h1>All Facilities</h1>
-           <p className="welcome-text">Find a sports facility for your next session.</p>
-        </main>
-    );
+  return (
+    <main className="welcome-screen">
+      <span className="webbrow">DISCOVER</span>
+      <h1>All Facilities</h1>
+      <p className="welcome-text">
+        Find a sports facility for your next session.
+      </p>
+    </main>
+  );
 };
 
 export default Facilities;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-
 const Home = () => {
-    const projectName= "PlayGrid"
-    return (
-         <main className="welcome-screen">
+  const projectName = "PlayGrid";
+  return (
+    <main className="welcome-screen">
       <span className="webbrow"> MERN STACK PROJECT</span>
 
       <h1>{projectName}</h1>
       <p className="welcome-text">Sports Facility Booking Management System</p>
       <button className="primary-button">Explore Facilities</button>
     </main>
-    );
+  );
 };
 
 export default Home;
